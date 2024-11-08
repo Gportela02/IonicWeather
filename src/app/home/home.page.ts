4ec6505d15c0c6bd28ef4c4ff0d65609ea1729f9
MarcioSobel <marcinhosobel@hotmail.com>
2024-11-07 23:47:31 -0300

    :recycle: refactor: refactor cacheing of weathers

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -27,9 +27,15 @@ export class HomePage implements OnInit {
   }
 
   searchWeathers(latitude: number = this.latitude, longitude: number = this.longitude) {
-    console.log("pesquisando")
     const weathersInCache = this.localStorage.getWeathersInCache();
     this.weathers = [...weathersInCache];
+
+    weathersInCache.forEach((weather, i) => {
+      this.openWeatherMapService.getCity(weather.coord.lat, weather.coord.lon).subscribe((data) => {
+        data.favorited = true;
+        this.weathers[i] = data;
+      });
+    });
 
     this.openWeatherMapService.getSurroundingCities(latitude, longitude, 0.1).subscribe(data => {
       data.forEach((weather) => {
@@ -37,42 +43,33 @@ export class HomePage implements OnInit {
         const alreadyFetched = cityNames.includes(weather.name);
         if (alreadyFetched) return;
 
-        const cacheCityNames = weathersInCache.map(w => w.name);
-        const alreadyInCache = cacheCityNames.includes(weather.name);
-
-        if (alreadyInCache) {
-          const index = cityNames.indexOf(weather.name);
-          this.weathers[index] = weather;
-        }
-
-        if (!alreadyInCache) {
-          weather.favorited = false;
-          weather.main.temp = Math.round(weather.main.temp);
-          this.weathers.push(weather);
-        }
-      })
-    })
+        weather.favorited = false;
+        this.weathers.push(weather);
+      });
+    });
   }
   
   setLatitude(input: string | number | null = null) {
     if (typeof input === "string") {
       input = parseFloat(input);
-    } else if (input === null) {
+      if (Number.isNaN(input)) return;
+    }
+    if (input === null) {
       input = this.defaultLatitude;
     }
     
-    if (Number.isNaN(input)) return;
     this.latitude = input;
   }
 
   setLongitude(input: string | number | null = null) {
     if (typeof input === "string") {
       input = parseFloat(input);
-    } else if (input === null) {
+      if (Number.isNaN(input)) return;
+    }
+    if (input === null) {
       input = this.defaultLongitude;
     }
     
-    if (Number.isNaN(input)) return;
     this.longitude = input;
   }
 
